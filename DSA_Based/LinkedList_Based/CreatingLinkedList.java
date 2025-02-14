f7f3b0b93a94437d6c897fde8171632d475ee58d
Smital D. Barage <smitalbarage1251@gmail.com>
2025-02-14 12:10:19 +0530

    Add files via upload

---
+++ DSA_Based/LinkedList_Based/CreatingLinkedList.java
@@ -0,0 +1,78 @@
+package Interview_basic.dsa.linkedList;
+
+
+
+
+
+public class CreatingLinkedList {
+
+	public static void main(String[] args) { 
+		
+		LinkedList list=new LinkedList();
+//		System.out.println("     Adding at Start:");
+//		
+//		
+//		
+//		list.display();
+//		
+//		System.out.println("     Adding at End:");
+//		list.insertAtEnd(11);
+//		list.display();
+//		
+//		
+//		System.out.println("     Adding at Start:");
+//		list.insertAtStart(20);
+//		list.display();
+//		
+		
+		char ch=40;
+		System.out.println((ch));
+		char ch1=(char)124;
+		System.out.println((ch1));
+		char ch2=(char)125;
+		System.out.println((ch2));
+		char ch3=(char)128;
+		System.out.println((ch3));
+		char ch4=(char)127;
+		System.out.println((ch4));
+		
+		
+		
+//		
+//        list.insertAtStart(3);
+//        list.insertAtStart(2);
+//        list.insertAtStart(8);
+//        list.insertAtStart(17);
+//        list.insertAtEnd(99);
+//        list.insert(100, 3);
+//        list.display();
+//        System.out.println(list.deleteFirst());
+//        list.display();
+//        System.out.println(list.deleteLast());
+//        list.display();
+//        System.out.println(list.delete(2));
+//        list.display();
+//        list.insertRec(88, 2);
+//        list.display();
+
+//       DoublyLinkedList list = new DoublyLinkedList();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//        list.insertLast(99);
+//        list.insert(8, 65);
+//
+//        list.display();
+
+//        CircularLinkedList list = new CircularLinkedList();
+//        list.insert(23);
+//        list.insert(3);
+//        list.insert(19);
+//        list.insert(75);
+//        list.display();
+//        list.delete(19);
+//        list.display();
+	}
+
+}
